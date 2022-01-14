0ce3b0fc54714d17acf7fc2e51484568af3bb530
YuriiTrofimenko <tyaa@ukr.net>
2022-01-14 22:00:34 +0200

    refactoring (step 1): model to dom subtree = view

--- main.mjs
+++ main.mjs
@@ -1,14 +1,5 @@
 import Student from './student.mjs'
-// document.getElementById('root').innerText = 'Hello DOM!'
-// Шаблон разметки отображения сведений об одном студенте
-const studentViewTemplate = `
-    <div>
-        <div><strong>Name: </strong>{{name}}</div>
-        <div>
-            <strong>Age: </strong><span>{{age}}</span>&nbsp;&nbsp;<strong>AVG Score: </strong><span>{{score}}</span>
-        </div>
-    </div>
-`
+
 // список моделей студентов
 const students = [
     new Student('John', 20, 10),
@@ -16,15 +7,24 @@ const students = [
     new Student('Mary', 22, 10.5)
 ]
 // заполнение представления списка данными моделей
-let studentsListView = ''
 students.forEach(s => {
-    const studentView = studentViewTemplate
-    studentsListView += studentView
-        .replace('{{name}}', s.name)
-        .replace('{{age}}', s.age)
-        .replace('{{score}}', s.avgScore)
+    const student = document.createElement('div')
+
+    const studentFirstDiv = document.createElement('div')
+    const studentNameTitle = document.createElement('strong')
+    studentNameTitle.innerText = 'Name: '
+    studentFirstDiv.append(studentNameTitle)
+    studentFirstDiv.append(s.name)
+
+    // TODO сконструировать фрагмент поддерева, соответствующий разметке второго вложенного div в представлении студента:
+    /*
+    * <div>
+            <strong>Age: </strong><span>{{age}}</span>&nbsp;&nbsp;<strong>AVG Score: </strong><span>{{score}}</span>
+        </div>
+    * */
+
+    student.append(studentFirstDiv)
+    // TODO вставить в корневой узел текущего студента сконструированный Вами выше узел второго вложенного div
+
+    document.getElementById('root').append(student)
 })
-// вставка заполненного предстваления списка на страницу
-// document.getElementById('root').innerText += studentDiv
-// document.getElementById('root').innerHTML += studentDiv
-document.getElementById('root').innerHTML = studentsListView
